df41d0e1ee4c656d5b07e4325412c7b1fd91c318
masumbillahtarek <86756549+masumbillahtarek@users.noreply.github.com>
2021-07-02 21:12:42 +0600

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,21 @@
+//mixit up
+
+var mixer = mixitup('.grid-portfolio-container');
+
+//scrollify
+//Humburger Menu
+function openMenu(){
+  document.getElementById('navbar').style.height="100%";
+}
+function closeMenu(){
+  document.getElementById('navbar').style.height="0%";
+}
+//smooth scroll
+$(function(){
+  $(".menu-item a,.scroll-down a").on("click",function(){
+    $("html,body").animate({
+      scrollTop:$($.attr(this,"href")).offset().top
+    },1000);
+    return false;
+  });
+});
